5cdd3afb94130c500b55c73ffae8c4b105c8a9c2
Eugenia-d <e.drobiazko@gmail.com>
2021-11-21 21:20:29 +0200

    rendering multiple countries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export const fetchCountries = name => {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,languages`,
+    `https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,languages,flag`,
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,60 @@
 import './css/styles.css';
+import 'reset-css';
 import _ from 'lodash';
 import { fetchCountries } from './fetchCountries';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 searchBox.addEventListener(
   'input',
   _.debounce(event => {
     const search = event.target.value.trim();
 
+    if (!search.length) {
+      return;
+    }
+
     fetchCountries(search)
       .then(data => {
+        cleanList();
         onDataReceived(data);
       })
       .catch(error => {
-        Notiflix.Notify.failure('Oops there is no country with that name');
+        cleanList();
+        Notify.failure('Oops there is no country with that name');
       });
   }, DEBOUNCE_DELAY),
 );
 
 const onDataReceived = array => {
-  if (array.length && array.length > 1) {
+  if (array.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (array.length > 1 && array.length <= 10) {
     renderList(array);
-  } else if (array.length == 1) {
+  } else if (array.length === 1) {
     renderCountry;
   }
 };
 
 const renderList = array => {
   console.log('rendering list: ', array);
+  const elements = array
+    .map(({ name, flag }) => {
+      return `<li id="country_item">${flag} ${name.common}</li>`;
+    })
+    .join();
+  countryList.insertAdjacentHTML('beforeend', elements);
 };
 
 const renderCountry = data => {
   console.log('rendering country: ', data);
+};
+
+const cleanList = () => {
+  countryList.innerHTML = '';
 };
